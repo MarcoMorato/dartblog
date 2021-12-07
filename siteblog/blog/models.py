c6caf54d07ce05b43c9e9c005d167f18aacde4f9
MarcoMorato <leoscorp969@yandex.ru>
2021-12-07 12:27:10 +0300

    зарегестрировал в admin.py классы моделей. исправил назавание модели Tegs на Teg

--- siteblog/blog/models.py
+++ siteblog/blog/models.py
@@ -16,17 +16,19 @@ title, slug, author, content, created_at, photo, views, category, tags
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name='Название')
     slug = models.SlugField(max_length=255, verbose_name='Url_category', unique=True)
 
     def __str__(self):
         return self.title
 
     class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
         ordering = ['title']
 
 
-class Tags(models.Model):
+class Tag(models.Model):
     title = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, verbose_name='Url_slug', unique=True)
 
@@ -46,7 +48,7 @@ class Post(models.Model):
     photo = models.ImageField(upload_to='photo/%Y/%m/%d/', blank=True)
     views = models.IntegerField(default=0, verbose_name='Кол-во просмотров')
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
-    tags = models.ManyToManyField(Tags, blank=True, related_name='posts')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
 
     def __str__(self):
         return self.title

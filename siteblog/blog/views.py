488f347ed2f64a699f3e57edc517dc8a5123859f
MarcoMorato <leoscorp969@yandex.ru>
2021-12-14 14:26:16 +0300

    статьи по категориям - PostByCategory -urls.py path

--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -3,11 +3,26 @@ from django.views.generic import ListView, DetailView
 from .models import Post, Category, Tag
 
 
+class PostByCategory(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(slug=self.kwargs['slug'])
+        return context
+
+
 class Home(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'posts'
-    paginate_by = 1
+    paginate_by = 4
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)

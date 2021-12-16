66ec869ee2d977ed347aa5aefa6c54e1a9dc62d2
MarcoMorato <leoscorp969@yandex.ru>
2021-12-16 20:59:29 +0300

    Получене статей по тегу

--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -5,7 +5,18 @@ from django.db.models import F
 
 
 class PostByTags(ListView):
-    pass
+    template_name = 'blog/index_category.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Записи по тегу' + str(Tag.objects.get(slug=self.kwargs['slug']))
+        return context
 
 
 class PostByCategory(ListView):

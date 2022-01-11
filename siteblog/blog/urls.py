9beeb4821bb652f0c72c87d26d6b73d322ec36bf
Jhon <leoscorp969@yandex.ru>
2022-01-11 14:41:48 +0300

    поиск по постам

--- siteblog/blog/urls.py
+++ siteblog/blog/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('category/<str:slug>/', PostByCategory.as_view(), name='category'),
-    path('post/<str:slug>/', GetPost.as_view(), name='post'),
     path('tag/<str:slug>/', PostByTags.as_view(), name='tag'),
+    path('post/<str:slug>/', GetPost.as_view(), name='post'),
+    path('search/', Search.as_view(), name='search'),
 ]
